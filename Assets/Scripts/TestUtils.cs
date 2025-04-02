b5eaa57db81d1b518645f2ce9adc8b416d75bae9
talon-ernst <63943404+talon-ernst@users.noreply.github.com>
2025-04-02 01:06:09 -0400

    Update TestUtils.cs
    Removing unnecessary comments

--- Assets/Scripts/TestUtils.cs
+++ Assets/Scripts/TestUtils.cs
@@ -6,11 +6,6 @@ namespace NOVA.Scripts
 {
     public static class TestUtils
     {
-        /// <summary>
-        ///     This coroutine is used to simulate clicking on a button on a test case
-        ///     Taken from Trades Electrical. Thank you to them :)
-        /// </summary>
-        /// <param name="button">The button to click</param>
         public static void ClickOnButton(Button button)
         {
             // Ensure that the button is enabled in the hierarchy
