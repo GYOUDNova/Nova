907677ae981dc7ad4db491ad1032616fbf847768
Omar <menesiena@proton.me>
2025-04-05 15:07:53 -0400

    feat(sqlite): add sqlite-net-pcl nuget package
    - This also adds the NuGetForUnity package through a git URL to manage NuGet packages in our Unity project easily
    - This also creates a basic Entity-Relationship mapping and a sample SqliteHandler that is initialized on load to be improved later on

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,40 @@
+using SQLite;
+using UnityEngine;
+
+namespace NOVA.Scripts
+{
+    [Table("Entity")]
+    public class Entity
+    {
+        [AutoIncrement, PrimaryKey]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [NotNull, MaxLength(64)]
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [NotNull, MaxLength(64)]
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [NotNull, MaxLength(64), Unique]
+        [Column("email")]
+        public string Email { get; set; }
+    }
+
+    [Table("RelationshipEntity")]
+    public class RelationshipEntity
+    {
+        [AutoIncrement, PrimaryKey]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Indexed]
+        [Column("entity_id")]
+        public int EntityId { get; set; }
+
+        [Ignore]
+        public Entity Entity { get; set; }
+    }
+}
