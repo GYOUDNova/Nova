6a7801a4a36c9a5bb37a212901ff3ac3aea66f59
Omar Nunez <menesiena@proton.me>
2025-04-30 20:15:30 -0400

    fix: add extra dotnet verbose format to bat script

--- fm.bat
+++ fm.bat
@@ -3,6 +3,7 @@ for /f "delims=" %%f in ('dir /b /a-d *.csproj') do (
     echo Verifying format for project: %%f
     dotnet format "%%f" --verify-no-changes --exclude "**/Mediapipe/**"
     echo Extra: Attempting to format the project
+    dotnet format -v diag "%%f" --exclude "**/Mediapipe/**" 
 )
 
 echo Review the log messages above for any errors.
